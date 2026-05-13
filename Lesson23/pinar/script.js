ef91b8e95f50f79bb6772a0ac0572212b8e53363
pinarertanc <pinarertanc@gmail.com>
2026-05-13 12:06:46 +0300

    Lesson23 notes are resolved and done

--- Lesson23/pinar/script.js
+++ Lesson23/pinar/script.js
@@ -2,27 +2,6 @@ const fetchButton = document.getElementById('fetchDataBtn');
 const errorMsg = document.getElementById('errorMsg');
 const randomImage = document.getElementById('randomImage');
 
-function getPosts() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `An error occured when fetching posts. Status: ${response.status}, status text: ${response.statusText}`,
-        );
-      }
-      console.log('Right after the error!');
-      return response.json();
-    })
-    .then((resultOfResponseJson) => console.log(resultOfResponseJson))
-    .catch((error) => {
-      console.error('An error occured.', error);
-      errorMsg.textContent = 'An error occured when fetching posts.';
-    })
-    .finally(() => {
-      document.getElementsByTagName('body')[0].classList.toggle('bg-dark');
-    });
-}
-
 function getRandomPhoto() {
   fetch(`https://api.unsplash.com/photos/random?orientation=portrait&query=sea&client_id=${UNSPLASH_CLIENT_ID}`)
     .then(
@@ -45,6 +24,9 @@ function getRandomPhoto() {
     .catch((error) => {
       console.error('An error occured.', error);
       errorMsg.textContent = 'An error occured when fetching random photo.';
+    })
+    .finally(() => {
+      document.body.classList.toggle('bg-dark')
     });
 }
 
