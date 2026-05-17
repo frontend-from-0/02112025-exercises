8fa3dcf5f62cf7aab1bc7e856dceaff9cd477acd
pinarertanc <pinarertanc@gmail.com>
2026-05-17 17:13:30 +0300

    Lesson24 practice has been completed

--- Lesson24/pinar/app.js
+++ Lesson24/pinar/app.js
@@ -1,20 +1,41 @@
-/* 
-CRUD - set of basic operations or functions that are commonly used in the context of database management and web applications to manage and manipulate data.
-C - create - POST method (has request body to transfer data)
-R - read - GET method (cannot have request body to send data to the server)
-U - update - PUT / PATCH method (have request body to transfer data)
-D - delete - DELETE method
-Status codes
-HTTP status codes are three-digit numbers that the server sends in response to a client's request made to a web server. They provide information about the outcome of the request, whether it was successful, encountered an error, or requires further action. HTTP status codes are grouped into several ranges, each indicating a different category of response. 
-100... - Informational Responses
-200... - Successful Responses (200 OK, 201 Created, 204 No content)
-300.. - redirection (301 Moved Permanently, Found (or 307 Temporary Redirect))
-400... - Errors (400 Bad Request, 401 Unauthorized, 403 Forbidden, 404 Not Found)
-500... - Service error (500 Internal Server Error, 502 Bad Gateway, 503 Service Unavailable)
-*/
+
 const container = document.getElementById('container');
 const fetchUsersButton = document.getElementById('fetchUsersButton');
 const closeButton = document.querySelector('.btn-close');
+
+function createUserCard(userObj){
+       const card = document.createElement('div');
+        card.classList.add('card');
+        card.id = `card-${userObj.id}`
+
+        const title = document.createElement('h2');
+        title.classList.add(`card-title`);
+        title.textContent = `User: ${userObj.id}`;
+
+        const paragraph = document.createElement('p');
+        paragraph.classList.add('card-body');
+        paragraph.textContent = `Name: ${userObj.firstName},   Last Name: ${userObj.lastName},   Age ${userObj.age}`;
+      
+        const buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('buttonDiv')
+        
+        const updateButton = document.createElement('a');
+        updateButton.classList.add('button', 'button--update');
+        updateButton.textContent = 'Update User';
+        updateButton.href = `./pages/update.html?userId=${userObj.id}`;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('button', 'button--danger');
+        deleteButton.textContent = 'Delete User';
+        deleteButton.addEventListener('click', () => deleteUser(userObj.id));
+
+        card.appendChild(title);
+        card.appendChild(paragraph);
+        buttonDiv.appendChild(updateButton);
+        buttonDiv.appendChild(deleteButton);
+        card.appendChild(buttonDiv);
+        container.appendChild(card);
+}
 
 function getUsers() {
   fetch('https://dummyjson.com/users')
@@ -25,46 +46,26 @@ function getUsers() {
       return res.json();
     })
     .then((data) => {
+      container.innerHTML = "";
       const users = data.users;
       users.forEach((user) => {
-      
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.id = `card-${user.id}`
 
-        const title = document.createElement('h2');
-        title.classList.add(`card-title`);
-        title.textContent = `User: ${user.id}`;
-
-        const paragraph = document.createElement('p');
-        paragraph.classList.add('card-body');
-        paragraph.textContent = `Name: ${user.firstName},   Last Name: ${user.lastName},   Age ${user.age}`;
-
-        const buttonDiv = document.createElement('div');
-        buttonDiv.classList.add('buttonDiv')
-        
-        const updateButton = document.createElement('a');
-        updateButton.classList.add('button', 'button--update');
-        updateButton.textContent = 'Update User';
-        updateButton.href = `./pages/update.html?userId=${user.id}`;
-
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('button', 'button--danger');
-        deleteButton.textContent = 'Delete User';
-        deleteButton.addEventListener('click', () => deleteUser(user.id));
-
-
-        card.appendChild(title);
-        card.appendChild(paragraph);
-        buttonDiv.appendChild(updateButton);
-        buttonDiv.appendChild(deleteButton);
-        card.appendChild(buttonDiv);
-
-        
-        container.appendChild(card);
+        const localData = localStorage.getItem(`updated_user_${user.id}`);
+        let userToDisplay = user;
+        if (localData) {
+          userToDisplay = JSON.parse(localData); 
+        }
+        createUserCard(userToDisplay);
       });
-    });
+    
+      const localNewUsers = JSON.parse(localStorage.getItem('new_users_list')) || [];
+      localNewUsers.forEach((newUser) => {
+        createUserCard(newUser);
+      });
+    })
+    .catch((err) => console.error(err.message)); 
 }
+    
 
 function deleteUser (userId) {
   const messageDiv = document.getElementById('messageDiv');
@@ -82,6 +83,7 @@ fetch(`https://dummyjson.com/users/${userId}`, {
    
    messageDiv.classList.remove('hidden');
    messageParagraph.textContent =`The user with id number ${user.id}, ${user.firstName} ${user.lastName} has been deleted succesfully.`; 
+   localStorage.removeItem(`updated_user_${userId}`);
    const deletedUser = document.getElementById(`card-${user.id}`);
    if(deletedUser){
    deletedUser.remove();}
--- Lesson24/pinar/pages/create.js
+++ Lesson24/pinar/pages/create.js
@@ -1,46 +1,25 @@
-const paramsString = window.location.search;
-const searchParams = new URLSearchParams(paramsString);
-const userId = searchParams.get(`userId`);
 
-const userIdInput = document.getElementById('id');
 const userFirstNameInput = document.getElementById('firstName');
 const userLastNameInput = document.getElementById('lastName');
 const userAgeInput = document.getElementById('age');
 
-const userUpdateButton = document.querySelector('.button--update');
+const createUserButton = document.getElementById('createUserButton');
 const closeButton = document.querySelector('.btn-close');
 
-if(userId){
-fetch(`https://dummyjson.com/users/${userId}`)
- .then(res => {
-  if (!res.ok) throw new Error("User doesn\'t exist.");
-      return res.json();
-})
- .then((user)=>{
-      const localData = localStorage.getItem(`updated_user_${userId}`);
-      let userToDisplay = user;
 
-      if (localData) {
-        userToDisplay = JSON.parse(localData);
-      }
-
-  userFirstNameInput.value = userToDisplay.firstName;
-  userLastNameInput.value = userToDisplay.lastName;
-  userAgeInput.value = userToDisplay.age;
-  userIdInput.value = userToDisplay.id;
- })
-}
-
-function updateUser(event){
+function createUser(event){
   if (event) event.preventDefault();
- const updatedUser = {
-  id: userIdInput.value,
+
+  const existingNewUsers = JSON.parse(localStorage.getItem('new_users_list')) || [];
+  const nextId = 30 + existingNewUsers.length + 1;
+
+  const newUser = {
+  id:nextId,  
   firstName: userFirstNameInput.value,
   lastName: userLastNameInput.value,
   age: userAgeInput.value,
+  }
 
- };
- const idPattern = /^[0-9]+$/;
  const firsNamePattern = /^[A-Za-zÇĞİıÖŞüçğiöşü\s]{2,30}$/;
  const lastNamePattern = /^[A-Za-zÇĞİıÖŞüçğiöşü]{2,30}$/;
  const agePattern = /^(1[0-1][0-9]|120|[1-9]?[0-9])$/;
@@ -53,13 +32,6 @@ function updateUser(event){
     p.classList.add('hidden');
     p.textContent = "";
 });
-
-if(!idPattern.test(userIdInput.value)){
- const validErrorElement = userIdInput.nextElementSibling;
-  validErrorElement.classList.remove('hidden');
-  validErrorElement.textContent = ("Please enter avalid id number consisting only numbers.");
- return;
-}
 
 if(!firsNamePattern.test(userFirstNameInput.value)){
   const validErrorElement = userFirstNameInput.nextElementSibling;
@@ -82,27 +54,29 @@ if(!agePattern.test(userAgeInput.value)){
   return;
 }
 
- fetch(`https://dummyjson.com/users/${userId}`, {
-  method: 'PUT',
+fetch('https://dummyjson.com/users/add', {
+  method: 'POST',
   headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(updatedUser)
+  body: JSON.stringify(newUser)
 })
 .then(res => {
   if(!res.ok) throw new Error(`An error occurred. Status: ${res.status}`)
     return res.json();
 })
-.then((updatedUser)=>{
+.then((data)=>{
   messageDiv.classList.remove('hidden');
-  messageParagraph.textContent = `The user with ${userId} id has been successfully updated.`;
-  localStorage.setItem(`updated_user_${userId}`, JSON.stringify(updatedUser));
+  messageParagraph.textContent = `The user with ${data.firstName} ${data.lastName} has been successfully created wit ID number ${nextId}.`;
+  existingNewUsers.push(newUser);
+  localStorage.setItem('new_users_list', JSON.stringify(existingNewUsers));
 })
 .catch(error=>{
   messageDiv.classList.remove('hidden');
   messageParagraph.textContent = `Something went wrong. ${error.message}`;
 })
 }
-  userUpdateButton.addEventListener('click', updateUser);
+  createUserButton.addEventListener('click', createUser);
   closeButton.addEventListener('click',()=>{
   messageDiv.classList.add('hidden');
   window.location.href = "../index.html";
+
 })
