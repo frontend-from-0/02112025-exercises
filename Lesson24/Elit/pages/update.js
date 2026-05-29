ae7edd8c70892cfd1db4ca3118711a336c1140ba
Elit-Isik <altunelit@gmail.com>
2026-05-29 12:59:17 +0300

    updating done. stage:create

--- Lesson24/Elit/pages/update.js
+++ Lesson24/Elit/pages/update.js
@@ -17,3 +17,80 @@ const successNotificationDiv = document.getElementById("success_notification");
 const errorNotificationDiv = document.getElementById("error_notification");
 const successMessageParagraph = document.getElementById("success_message");
 const errorMessageParagraph = document.getElementById("error_message");
+
+function getUserDetails() {
+  if (!userId) {
+    errorMessageParagraph.textContent = "User ID not found in URL!";
+    errorNotificationDiv.classList.remove("hidden");
+    return;
+  }
+
+  fetch(`https://dummyjson.com/users/${userId}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Failed to fetch user details. Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((user) => {
+      firstNameInput.value = user.firstName;
+      lastNameInput.value = user.lastName;
+      ageInput.value = user.age;
+      console.log("Form successfully prefilled with user data", user);
+    })
+    .catch((error) => {
+      console.log("Error fetching user details:", error);
+      errorMessageParagraph.textContent =
+        "Could not load user details. Please try again.";
+      errorNotificationDiv.classList.remove("hidden");
+    });
+}
+getUserDetails();
+
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const updatedData = {
+    firstName: firstNameInput.value.trim(),
+    lastName: lastNameInput.value.trim(),
+    age: parseInt(ageInput.value, 10),
+  };
+
+  if (
+    !updatedData.firstName ||
+    !updatedData.lastName ||
+    isNaN(updatedData.age)
+  ) {
+    errorMessageParagraph.textContent = "Please fill out all fields correctly!";
+    errorNotificationDiv.classList.remove("hidden");
+    return;
+  }
+
+  fetch(`https://dummyjson.com/users/${userId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(updatedData),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("`Failed to update user. Status: ${res.status}`");
+      }
+      return res.json();
+    })
+    .then((updatedData) => {
+      console.log("User updated successfully on API:", updatedData);
+      successMessageParagraph.textContent = `User ${updatedData.firstName} ${updatedData.lastName} has been successfully updated.`;
+      successNotificationDiv.classList.remove("hidden");
+      errorNotificationDiv.classList.add("hidden");
+
+      setTimeout(() => {
+        window.location.href = "../index.html";
+      }, 5000);
+    })
+    .catch((error) => {
+      console.log("Error updating user:", error);
+      errorMessageParagraph.textContent =
+        "An error occurred while updating the user. Please try again.";
+      errorNotificationDiv.classList.remove("hidden");
+    });
+});
