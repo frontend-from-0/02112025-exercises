f88ea8e4588d42f23b61ad9d3fa44cd584cc6db6
Kaysi6 <furkan.kay432@gmail.com>
2026-07-20 17:43:59 +0300

    Refactor script.js: Remove commented-out code and implement arrow functions with default parameters

--- Lesson27/furkan/app.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('clickMeBtn');
-button.addEventListener('click', () => printHello('Furkan'));
-
-
-function printHello(name = 'Guest') {
-  console.log('Hello', name);
-}
